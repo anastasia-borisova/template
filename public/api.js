3578a07426dc88cc53a24d517909800f7fdc3a0c
ad.borisova <borisova_anastasia99@mail.ru>
2022-05-31 21:44:26 +0300

    Исправления JS

--- public/api.js
+++ public/api.js
@@ -1,113 +1,105 @@
 export { getCategories, getToken, getPlaylists, search }
 
-/**
- * Функция для получения токена доступа 
- * При возникновении ошибки в консоль выводится сообщение
- * @returns токен доступа 
-*/
-async function getToken() {
-  const client_id = 'cc54ff5069b04ecbb4cf473f445e9872';
-  const client_secret = '5a84d500c04542fe87ca70dee98313fc';
-  try {
-    const response = await fetch('https://accounts.spotify.com/api/token', {
+//класс для создания дополнительных настроек запроса
+class Options {
+
+  constructor(token) {
+    this.token = token;
+  }
+
+  getOptions() {
+    return {
+      method: 'GET',
+      headers: { 
+        'Authorization' : 'Bearer ' + this.token,
+        'Content-Type' : 'application/json',
+      }
+    };
+  }
+
+  postOptions(client_id, client_secret) {
+    return {
       method: 'POST',
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded',
         'Authorization': 'Basic ' + btoa(client_id + ':' + client_secret)
       },
       body: 'grant_type=client_credentials'
-    })
-    .then((res) => {
-      if(!res.ok)
-        throw new Error(`Error ${res.status}`);
-      else
-        return res.json();
-    });
-    return response.access_token;
+    }
+  }
+}
+
+/**
+ * Функция для выполнения запросов к api
+ * При возникновении ошибки в консоль выводится сообщение
+ * @param {string} url - адрес, по которому выполняется запрос
+ * @param {Options} init - дополнительные параметры для настройки запроса
+ * @returns ответ на запрос в формате json
+ */
+async function queryToApi(url, init) {
+  try {
+    const response = await fetch(url, init);
+    if(!response.ok)
+        throw new Error(`Error ${response.status}`);
+    else {
+      const json = await response.json();
+      return json;
+    }
   }
   catch(error) {
     console.log(error.message);
   }
+}
+
+/**
+ * Функция для получения токена доступа 
+ * @returns объект, содержащий токен доступа и время его жизни
+*/
+async function getToken() {
+  const client_id = 'cc54ff5069b04ecbb4cf473f445e9872';
+  const client_secret = '5a84d500c04542fe87ca70dee98313fc';
+  const url = 'https://accounts.spotify.com/api/token';
+  const options = new Options();
+  const response = await queryToApi(url, options.postOptions(client_id, client_secret));
+  return response;
 }
 
 /**
  * Функция для получения категорий плейлистов Spotify
- * При возникновении ошибки в консоль выводится сообщение
  * @param {string} token - токен доступа
  * @returns массив категорий плейлистов Spotify
  */
 async function getCategories(token) {
-  try {
-    const response = await fetch('https://api.spotify.com/v1/browse/categories', {
-      method: 'GET',
-      headers: { 
-        'Authorization' : 'Bearer ' + token,
-        'Content-Type' : 'application/json'
-        },
-    })
-    .then((res) => {
-      if(!res.ok)
-        throw new Error(`Error ${res.status}`);
-      else
-        return res.json();
-    });
+  const url = 'https://api.spotify.com/v1/browse/categories?limit=10&country=US';
+  const options = new Options(token);
+  const response = await queryToApi(url, options.getOptions());
+  if(response && response.categories)
     return response.categories.items;
-  }
-  catch(error) {
-    console.log(error.message);
-  }
 }
 
 /**
 * Функция для получения плейлистов по идентификатору категории
-* При возникновении ошибки в консоль выводится сообщение
 * @param {string} token - токен доступа
 * @param {string} category_id - категория плейлиста
 * @returns массив плейлистов, входящих в категорию category_id
 */
 async function getPlaylists(token, category_id) {
-  try {
-    const response = await fetch(`https://api.spotify.com/v1/browse/categories/${category_id}/playlists?limit=10`, {
-      method: 'GET',
-      headers: { 'Authorization' : 'Bearer ' + token },
-    })
-    .then((res) => {
-      if(!res.ok)
-        throw new Error(`Error ${res.status}`);
-      else
-        return res.json();
-    });
+  const url = `https://api.spotify.com/v1/browse/categories/${category_id}/playlists?limit=10&country=US`;
+  const options = new Options(token);
+  const response = await queryToApi(url, options.getOptions());
+  if(response && response.playlists)
     return response.playlists.items;
-  }
-  catch(error) {
-    console.log(error.message);
-  }
 }
 
 /**
 * Функция для получения альбомов, исполнителей и треков, соответствующих поисковому запросу
-* При возникновении ошибки в консоль выводится сообщение
 * @param {string} token - токен доступа 
 * @param {string} searchQuery - поисковый запрос
 * @returns объект, содержащий альбомы, исполнителей и треки, которые соответствуют поисковому запросу
 */
 async function search(token, searchQuery) {
-  try {
-    const response = await fetch(`https://api.spotify.com/v1/search?q=${searchQuery}&type=album,artist,track`, {
-    method: 'GET',
-    headers: { 'Authorization' : 'Bearer ' + token }
-    })
-    .then((res) => {
-      if(!res.ok)
-        throw new Error(`Error ${res.status}`);
-      else
-        return res.json();
-    });
-    return response;
-  }
-  catch(error) {
-    console.log(error.message);
-  }
+  const url = `https://api.spotify.com/v1/search?q=${searchQuery}&type=album,artist,track`;
+  const options = new Options(token);
+  const response = await queryToApi(url, options.getOptions());
+  return response;
 }
-
-
--- public/script.js
+++ public/script.js
@@ -10,7 +10,7 @@ const input = document.querySelector('.header__search');
  * @returns html элемент section, состоящего из h2 - заголовка и div - контейнера для хранения элементов
  */
 function createSection() {
-  let section = document.createElement('section');
+  const section = document.createElement('section');
   section.className = 'section';
   const template = `
   <h2 class="section__header"></h2>
@@ -28,6 +28,7 @@ function objectToHtmlElement(obj) {
   const a = document.createElement('a');
   a.tabIndex = 0;
   a.classList = 'item link footer__item-link';
+  a.target= '_blank';
   a.href = obj.external_urls.spotify;
   const image_url = obj.images.length == 0 ? "photos/default.jpg" : obj.images[0].url;
   const template = `
@@ -45,13 +46,15 @@ function objectToHtmlElement(obj) {
 * @returns раздел, содержащий название категории и ссылки на плейлисты из категории
 */
 function playlistsToHtmlElement(playlists, categoryName) {
-  let playlistSection = createSection().cloneNode(true);
+  const playlistSection = createSection().cloneNode(true);
   playlistSection.querySelector('.section__header').textContent = categoryName;
+  const fragment = document.createDocumentFragment();
   playlists.forEach(playlist => {
-    let element = objectToHtmlElement(playlist);
+    const element = objectToHtmlElement(playlist);
     element.insertAdjacentHTML('beforeend', `<p>${playlist.description}</p>`);
-    playlistSection.querySelector('.items').insertAdjacentElement('beforeend', element);
+    fragment.appendChild(element);
   });
+  playlistSection.querySelector('.items').appendChild(fragment);
   return playlistSection;
 }
 
@@ -77,13 +80,15 @@ function artistsToString(artists) {
 * @returns раздел, содержащий ссылки на альбомы
 */
 function albumsToHtmlElement(albums) {
-  let albumSection = createSection().cloneNode(true);
+  const albumSection = createSection().cloneNode(true);
   albumSection.querySelector('.section__header').textContent = 'Альбомы';
+  const fragment = document.createDocumentFragment();
   albums.forEach(album => {
-    let element = objectToHtmlElement(album);
+    const element = objectToHtmlElement(album);
     element.insertAdjacentHTML('beforeend', `<p>${artistsToString(album.artists)}</p>`);
-    albumSection.querySelector('.items').insertAdjacentElement('beforeend', element);
+    fragment.appendChild(element);
   });
+  albumSection.querySelector('.items').appendChild(fragment);
   return albumSection;
 }
 
@@ -93,11 +98,13 @@ function albumsToHtmlElement(albums) {
 * @returns раздел, содержащий ссылки на исполнителей
 */
 function artistsToHtmlElement(artists) {
-  let artistSection = createSection().cloneNode(true);
+  const artistSection = createSection().cloneNode(true);
   artistSection.querySelector('.section__header').textContent = 'Исполнители';
+  const fragment = document.createDocumentFragment();
   artists.forEach(artist => {
-    artistSection.querySelector('.items').insertAdjacentElement('beforeend', objectToHtmlElement(artist));
+    fragment.appendChild(objectToHtmlElement(artist));
   });
+  artistSection.querySelector('.items').appendChild(fragment);
   return artistSection;
 }
 
@@ -107,13 +114,13 @@ function artistsToHtmlElement(artists) {
 * @returns раздел, содержащий ссылки на треки
 */
 function tracksToHtml(tracks) {
-  let trackSection = createSection().cloneNode(true);
+  const trackSection = createSection().cloneNode(true);
   trackSection.querySelector('.section__header').textContent = 'Треки';
   trackSection.className = 'track-section';
   trackSection.querySelector('.items').className = 'tracks';
   tracks.forEach(track => {
     trackSection.querySelector('.tracks').insertAdjacentHTML('beforeend', 
-    `<a href='${track.external_urls.spotify}' 
+    `<a href='${track.external_urls.spotify}' target='_blank'
      class='track link'>${artistsToString(track.artists)} - ${track.name}</a>`);
   });
   return trackSection;
@@ -128,15 +135,17 @@ function searchResults(obj) {
   const albums = obj.albums.items;
   const artists = obj.artists.items;
   const tracks = obj.tracks.items;
-  if(albums.length == 0 && artists.length == 0 && tracks.length == 0)
+  if(albums.length === 0 && artists.length === 0 && tracks.length === 0)
     content.insertAdjacentHTML('afterbegin', '<div class="not-found">По вашему запросу ничего не найдено</div>');
   else {
-    if(albums.length != 0)
-      content.insertAdjacentElement('beforeend', albumsToHtmlElement(albums));
-    if(artists.length != 0)
-      content.insertAdjacentElement('beforeend', artistsToHtmlElement(artists));
-    if(tracks.length != 0)
-      content.insertAdjacentElement('beforeend', tracksToHtml(tracks));
+    const fragment = document.createDocumentFragment();
+    if(albums.length !== 0)
+      fragment.appendChild(albumsToHtmlElement(albums));
+    if(artists.length !== 0)
+      fragment.appendChild(artistsToHtmlElement(artists))
+    if(tracks.length !== 0)
+      fragment.appendChild(tracksToHtml(tracks));
+    content.appendChild(fragment);
   }
 }
 
@@ -148,14 +157,20 @@ function searchResults(obj) {
  * @param {number} end - индекс в массиве категорий плейлистов, до которого происходит вывод
  */
 async function showPlaylists(start, end) {
-  const token = await getToken();
-  const categories = await getCategories(token);
-  if(typeof(end) == 'undefined')
-    end = categories.length;
-  for(let i = start; i < end; i++) {
-    const playlists = await getPlaylists(token, categories[i].id);
-    if(typeof(playlists) != 'undefined' && playlists.length > 0) 
-      content.insertAdjacentElement('beforeend', playlistsToHtmlElement(playlists, categories[i].name));
+  clearContent();
+  addButton();
+  const token = localStorage.getItem('token');
+  if(token) {
+    const categories = await getCategories(token);
+    if(!end)
+      end = categories.length;
+    const fragment = document.createDocumentFragment();
+    for(let i = start; i < end; i++) {
+      const playlists = await getPlaylists(token, categories[i].id);
+      if(playlists && playlists.length > 0) 
+        fragment.appendChild(playlistsToHtmlElement(playlists, categories[i].name));
+    }
+  content.appendChild(fragment);
   }
 }
 
@@ -170,7 +185,7 @@ function hidePlaylists() {
 }
 
 content.addEventListener('click', (event) => {
-  if(event.target.className == 'show-all') {
+  if(event.target.classList.contains('show-all')) {
     if(event.target.textContent == 'Показать все') {
       event.target.textContent = 'Скрыть';
       showPlaylists(5);
@@ -182,68 +197,64 @@ content.addEventListener('click', (event) => {
   }
 });
 
+let keyPressTimeout = null;
+
 async function keyPress(searchQuery) {
-  const token = await getToken();
-  const queryResult = await search(token, searchQuery);
-  searchResults(queryResult);
+  keyPressTimeout = null;
+  const token = localStorage.getItem('token');
+  if(token) {
+    const queryResult = await search(token, searchQuery);
+    searchResults(queryResult);
+  }
 }
 
 input.addEventListener('keypress', () => {
-  if(input.value.length > 1) {
-    keyPress(input.value);
+  if(keyPressTimeout) {
+    clearTimeout(keyPressTimeout);
   }
+  keyPressTimeout = setTimeout(() => {
+    keyPress(input.value);
+  }, 150);
 });
 
 input.addEventListener('search', () => {
   content.innerHTML = '';
-  content.insertAdjacentHTML('afterbegin', '<div class="button-container"><button type="button" class="show-all">Показать все</button></div>')
+  addButton();
   showPlaylists(0, 5);
-})
+});
 
-showPlaylists(0, 5);
+function showTokenError(errorMessage) {
+  content.innerHTML = '';
+  content.insertAdjacentHTML("afterbegin", `<div class='error'>Сайт временно недоступен<p>${errorMessage}</p></div>`);
+}
 
+function addButton() {
+  if(!document.querySelector(".show-all"))
+    content.insertAdjacentHTML('afterbegin', `<div class='button-container'><button type='button'` +
+    `class='show-all'>Показать все</button></div>`);
+}
 
+function clearContent() {
+  if(document.querySelector(".error"))
+    content.innerHTML = '';
+}
 
+async function setToken() {
+  const token = await getToken();
+  if(token.error) {
+    localStorage.clear();
+    localStorage.setItem('time', 0);
+    showTokenError(token.error);
+  }
+  else {
+    localStorage.setItem('token', token.access_token);
+    localStorage.setItem('time', token.expires_in);
+    showPlaylists(0, 5);
+  }
+}
 
+setToken();
 
-
-
-
-
-
-
-
-
-
-
-
-  
-  
-
-
-
-
-    
-
-    
-
-    
-
-
-
- 
-
-
-  
-
-  
-
-
-
-
-
-
-
-
-
-
+setInterval(() => {
+  setToken();
+}, parseInt(localStorage.getItem('time')) * 1000);
